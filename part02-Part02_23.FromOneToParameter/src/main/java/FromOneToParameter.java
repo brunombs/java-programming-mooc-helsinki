1bfa5157669a3c6333b126aed27497573c1796b2
brunombs <brunozs@msn.com>
2024-12-04 07:05:48 -0300

    adding exercises

--- part02-Part02_23.FromOneToParameter/src/main/java/FromOneToParameter.java
+++ part02-Part02_23.FromOneToParameter/src/main/java/FromOneToParameter.java
@@ -1,9 +1,15 @@
-
+import java.util.Scanner;
 
 public class FromOneToParameter {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        printUntilNumber(7);
     }
 
+    public static void printUntilNumber(int number){
+        for (int i = 1; i <= number; i++){
+            System.out.println(i);
+        }
+    }
 }
