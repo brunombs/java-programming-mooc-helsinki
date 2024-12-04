b9413003e7712eca7e8ed5d69e0c489677ae459a
brunombs <brunozs@msn.com>
2024-12-04 04:39:59 -0300

    adding exercise

--- part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
+++ part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
@@ -5,6 +5,18 @@ public class NumberOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int total = 0;
 
+        while(true){
+            System.out.println("Give a number:");
+            int number = scanner.nextInt();
+            if (number == 0){
+                break;
+            } else {
+                total = total+1;
+            }
+        }
+
+        System.out.println("Number of numbers: " + total);
     }
 }
