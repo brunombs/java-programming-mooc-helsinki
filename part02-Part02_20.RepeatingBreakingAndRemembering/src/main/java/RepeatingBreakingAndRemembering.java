5bb909e396c0bc30046624c0b1c0e726e120884b
brunombs <brunozs@msn.com>
2024-12-04 06:36:48 -0300

    adding exercises

--- part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
+++ part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
@@ -6,13 +6,18 @@ public class RepeatingBreakingAndRemembering {
     public static void main(String[] args) {
         
         Scanner scanner = new Scanner(System.in);
-
+        int sum = 0;
         int number = 0;
         System.out.println("Give numbers:");
         while (number != -1){
             number = scanner.nextInt();
+            if (number == -1){
+                break;
+            }
+            sum += number;
         }
 
         System.out.println("Thx! Bye!");
+        System.out.println("Sum: " + sum);
     }
 }
