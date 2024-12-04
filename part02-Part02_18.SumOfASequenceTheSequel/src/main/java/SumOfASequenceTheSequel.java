188be93cd3b21728ffc0efa115904d9e690ae47d
brunombs <brunozs@msn.com>
2024-12-04 06:12:05 -0300

    adding exercises

--- part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
+++ part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
@@ -5,6 +5,18 @@ public class SumOfASequenceTheSequel {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        int sum = 0;
+        System.out.print("First number? ");
+        int first = scanner.nextInt();
+        System.out.print("Last number? ");
+        int last = scanner.nextInt();
 
+        for (int i = first; i <= last; i++){
+            System.out.println(i);
+            sum += i;
+        }
+
+        System.out.println("The sum is: " + sum);
     }
 }
