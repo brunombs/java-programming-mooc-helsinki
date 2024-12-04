d626658c1a32ebe9fb12819e9187323347e41403
brunombs <brunozs@msn.com>
2024-12-04 06:42:41 -0300

    adding exercises

--- part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
+++ part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
@@ -9,11 +9,17 @@ public class RepeatingBreakingAndRemembering {
         int sum = 0;
         int numbers = 0;
         int number = 0;
+        int odd = 0;
+        int even = 0;
         System.out.println("Give numbers:");
-        while (number != -1){
+        while (true){
             number = scanner.nextInt();
             if (number == -1){
                 break;
+            } else if(number % 2 == 0){
+                even++;
+            } else {
+                odd++;
             }
             sum += number;
             numbers++;
@@ -23,5 +29,7 @@ public class RepeatingBreakingAndRemembering {
         System.out.println("Sum: " + sum);
         System.out.println("Numbers: " + numbers);
         System.out.println("Average: " + ((double) sum/numbers));
+        System.out.println("Even: " + even);
+        System.out.println("Odd:" + odd);
     }
 }
