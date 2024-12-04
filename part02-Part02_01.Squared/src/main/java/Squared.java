202982ec0d7a579b970455b2c976220b9c2b7916
brunombs <brunozs@msn.com>
2024-12-04 04:00:00 -0300

    adding exercise

--- part02-Part02_01.Squared/src/main/java/Squared.java
+++ part02-Part02_01.Squared/src/main/java/Squared.java
@@ -6,5 +6,8 @@ public class Squared {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int number = scanner.nextInt();
+        int result = number * number;
+        System.out.println(result);
     }
 }
