97f5c2bc4cf1e13773f6925993416107f0043ae5
brunombs <brunozs@msn.com>
2024-12-04 06:16:20 -0300

    adding exercises

--- part02-Part02_19.Factorial/src/main/java/Factorial.java
+++ part02-Part02_19.Factorial/src/main/java/Factorial.java
@@ -6,5 +6,14 @@ public class Factorial {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int factorial = 1;
+        System.out.print("Give a number: ");
+        int number = scanner.nextInt();
+        for (int i = 1; i <= number; i++){
+            System.out.println(i);
+            factorial *= i;
+        }
+
+        System.out.println("Factorial: " + factorial);
     }
 }
