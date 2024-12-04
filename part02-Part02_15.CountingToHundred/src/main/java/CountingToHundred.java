e7a8f6687bb60144d4c08405167c3cb377b0715b
brunombs <brunozs@msn.com>
2024-12-04 05:46:12 -0300

    adding exercises

--- part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
+++ part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
@@ -5,6 +5,11 @@ public class CountingToHundred {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int end = 100;
 
+        int number = scanner.nextInt();
+        for (int i = number; i <= end; i++){
+            System.out.println(i);
+        }
     }
 }
