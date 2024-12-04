dc9ba3cd4f5e119e4786df7a89bcf164135d564e
brunombs <brunozs@msn.com>
2024-12-04 04:54:30 -0300

    adding exercise

--- part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
+++ part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
@@ -6,5 +6,21 @@ public class NumberAndSumOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int total = 0;
+        int sum = 0;
+
+        while(true){
+            System.out.println("Give a number:");
+            int number = scanner.nextInt();
+            if (number == 0){
+                break;
+            } else {
+                total += 1;
+                sum += number;
+            }
+        }
+
+        System.out.println("Number of numbers: " + total);
+        System.out.println("Sum of the numbers: " + sum);
     }
 }
