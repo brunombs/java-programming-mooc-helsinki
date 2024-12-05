26ae084377dc3b8da403e3e9baca49bbf49d4b7f
brunombs <brunozs@msn.com>
2024-12-05 15:20:43 -0300

    adding exercise

--- part03-Part03_10.IndexOf/src/main/java/IndexOf.java
+++ part03-Part03_10.IndexOf/src/main/java/IndexOf.java
@@ -19,6 +19,12 @@ public class IndexOf {
 
         System.out.println("");
 
-        // implement here finding the indices of a number
+        System.out.print("Search for? ");
+        int number = scanner.nextInt();
+        for(int i = 0; i < list.size(); i++){
+            if (list.get(i) == number){
+                System.out.println(number + " is at index " + i);
+            }
+        }
     }
 }
