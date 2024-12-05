551e3cb2732883cd2379e323251056dcca9308eb
brunombs <brunozs@msn.com>
2024-12-05 04:38:30 -0300

    adding exercise

--- part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
+++ part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
@@ -2,19 +2,37 @@
 public class AdvancedAstrology {
 
     public static void printStars(int number) {
-        // part 1 of the exercise
+        while(number > 0){
+            System.out.print("*");
+            number -= 1;
+        }
+
+        System.out.println("");
     }
 
     public static void printSpaces(int number) {
-        // part 1 of the exercise
+        for (int i = 0; i < number; i++){
+            System.out.print(" ");
+        }
     }
 
     public static void printTriangle(int size) {
-        // part 2 of the exercise
+        for (int i = 0; i< size; i++){
+            printSpaces(size -i -1);
+            printStars(i + 1);
+        }
     }
 
     public static void christmasTree(int height) {
-        // part 3 of the exercise
+        for(int i = 0; i< height; i++){
+            printSpaces(height -i -1);
+            printStars(2 * i + 1);
+        }
+
+        for (int i= 0; i< 2; i++){
+            printSpaces(height -2);
+            printStars(3);
+        }
     }
 
     public static void main(String[] args) {
