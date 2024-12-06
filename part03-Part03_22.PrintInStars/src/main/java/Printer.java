49b6ed0392f75f232e5d91e1e014faf30efff224
brunombs <brunozs@msn.com>
2024-12-06 06:00:52 -0300

    adding exercise

--- part03-Part03_22.PrintInStars/src/main/java/Printer.java
+++ part03-Part03_22.PrintInStars/src/main/java/Printer.java
@@ -2,7 +2,6 @@
 public class Printer {
 
     public static void main(String[] args) {
-        // You can test the method here
         int[] array = {5, 1, 3, 4, 2};
         printArrayInStars(array);
     }
