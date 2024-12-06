7eb4c7588e22710366d578f3328d895b80684043
brunombs <brunozs@msn.com>
2024-12-06 05:47:30 -0300

    adding exercise

--- part03-Part03_20.SumOfArray/src/main/java/SumOfArray.java
+++ part03-Part03_20.SumOfArray/src/main/java/SumOfArray.java
@@ -2,7 +2,6 @@
 public class SumOfArray {
 
     public static void main(String[] args) {
-        // You can try the method here
         int[] array = {5, 1, 3, 4, 2};
         System.out.println(sumOfNumbersInArray(array));
     }
