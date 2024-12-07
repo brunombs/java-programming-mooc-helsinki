19e6ae52803cce97ff9555ff9c75197930043c14
brunombs <brunozs@msn.com>
2024-12-07 19:38:29 -0300

    adding exercise

--- part03-Part03_26.LineByLine/src/main/java/LineByLine.java
+++ part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -5,13 +5,21 @@ public class LineByLine {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String text = scanner.nextLine();
-        while (text != ""){
-            String[] lines = text.split(" ");
-            for (String line : lines){
-                System.out.println(line);
+
+        
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            String[] splitStringArray = userInput.split(" ");
+
+            for (String e : splitStringArray) {
+                System.out.println(e);
             }
-            text = scanner.nextLine();
+            if (userInput.equals("")) {
+                break;
+            }
+
         }
+
     }
 }
