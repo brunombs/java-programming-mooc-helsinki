b4204823815814b31036847e04fe4fda706d2607
brunombs <brunozs@msn.com>
2024-12-07 19:59:16 -0300

    adding exercise

--- part03-Part03_28.FirstWords/src/main/java/FirstWords.java
+++ part03-Part03_28.FirstWords/src/main/java/FirstWords.java
@@ -6,6 +6,13 @@ public class FirstWords {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        while (true){
+            String input = scanner.nextLine();
+            String text[] = input.split(" ");
+            System.out.println(text[0]);
+            if (input.equals("")){
+                break;
+            }
+        }
     }
 }
