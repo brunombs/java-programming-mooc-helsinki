7e9d1a3cdb47161a8c343c8821d4413c33606bf7
brunombs <brunozs@msn.com>
2024-12-07 19:44:16 -0300

    adding exercise

--- part03-Part03_27.AVClub/src/main/java/AVClub.java
+++ part03-Part03_27.AVClub/src/main/java/AVClub.java
@@ -5,7 +5,22 @@ public class AVClub {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
 
+        while(true){
+            String[] lines = text.split(" ");
+            for (String line : lines){
+                if (line.contains("av")){
+                    System.out.println(line);
+                }
+            }
+
+            if (text.equals("")){
+                break;
+            }
+
+            text = scanner.nextLine();
+        }
 
     }
 }
