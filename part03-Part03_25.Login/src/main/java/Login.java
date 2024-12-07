7460e840140ca2c11d1c499a675edc478bdf9b68
brunombs <brunozs@msn.com>
2024-12-07 19:15:45 -0300

    adding exercise

--- part03-Part03_25.Login/src/main/java/Login.java
+++ part03-Part03_25.Login/src/main/java/Login.java
@@ -6,5 +6,21 @@ public class Login {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String[] users = {"alex", "emma"};
+        String[] passwords = {"sunshine", "haskell"};
+        System.out.print("Enter username: ");
+        String name = scanner.nextLine();
+        System.out.print("Enter password: ");
+        String password = scanner.nextLine();
+        boolean foundUser = false;
+        for (int i = 0; i < users.length; i++){
+            if (name.equals(users[i]) && password.equals(passwords[i])){
+                System.out.println("You have successfully logged in!");
+                foundUser = true;
+            }   
+        }
+        if(!foundUser){
+            System.out.println("Incorrect username or password!");
+        }
     }
 }
