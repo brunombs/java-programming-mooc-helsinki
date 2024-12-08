bc07b4523dce76d57230d65b9c08cdc562f349c1
brunombs <brunozs@msn.com>
2024-12-08 02:03:14 -0300

    adding exercise

--- part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
+++ part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
@@ -5,7 +5,20 @@ public class AgeOfTheOldest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int AgeOfTheOldest = 0;
 
+        while(true){
+            String input = scanner.nextLine();
+            if (input.equals("")){
+                break;
+            }
 
+            String[] parts = input.split(",");
+            System.out.println(parts[1]);
+            if (Integer.valueOf(parts[1]) > AgeOfTheOldest){
+                AgeOfTheOldest = Integer.valueOf(parts[1]);
+            }
+        }
+        System.out.println("Age of the oldest: " + AgeOfTheOldest);
     }
 }
