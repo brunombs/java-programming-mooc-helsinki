ea6658d0662b7509b9d86551872fdbce2bbda773
brunombs <brunozs@msn.com>
2024-12-09 01:54:29 -0300

    adding exercise (:

--- part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -6,7 +6,28 @@ public class PersonalDetails {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String longestName = "";
+        int divisible = 0;
+        Double averageOfBirth = 0.0;
 
+        while(true){
+            String input = scanner.nextLine();
+            if (input.equals("")){
+                break;
+            }
+            divisible++;
 
+            String[] details = input.split(",");
+
+            averageOfBirth += Double.valueOf(details[1]);
+            
+            if(details[0].length() > longestName.length()){
+                longestName = details[0];
+            }
+        }
+
+        System.out.println("Longest name: " + longestName);
+        averageOfBirth /= divisible;
+        System.out.println("Average of the birth years: " + averageOfBirth);
     }
 }
