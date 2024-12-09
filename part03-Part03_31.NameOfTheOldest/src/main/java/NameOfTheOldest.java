a3e0318df56c38a41055230b5a6c45323455ea7f
brunombs <brunozs@msn.com>
2024-12-09 01:08:52 -0300

    adding exercise (:

--- part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -5,7 +5,28 @@ public class NameOfTheOldest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String nameOfOldest = "";
+        int older = 0;
+
+        while(true){
+            String nameAndAge = scanner.nextLine();
 
 
+            if(nameAndAge.equals("")){
+                break;
+            }
+
+
+            String[] list = nameAndAge.split(",");
+
+            String name = list[0];
+
+            if(Integer.valueOf(list[1]) > older){
+                older = Integer.valueOf(list[1]);
+                nameOfOldest = name;
+            }
+        }
+        
+        System.out.println("Name of the oldest: " + nameOfOldest);
     }
 }
