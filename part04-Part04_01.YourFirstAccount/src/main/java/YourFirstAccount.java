7d64c812520001d8d43a32407ed69bc0e2f65c55
brunombs <brunozs@msn.com>
2024-12-09 03:20:42 -0300

    adding exercise (:

--- part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -3,6 +3,8 @@ public class YourFirstAccount {
 
     public static void main(String[] args) {
         // Do not touch the code in Account.java
-        // Write your program here
+        Account account = new Account("Bruno", 100);
+        account.deposit(20);
+        System.out.println(account);
     }
 }
