5f11c4d8c53a7ca654221f1f7fbaf9c4c486da89
brunombs <brunozs@msn.com>
2024-12-12 07:49:04 -0300

    adding exercise

--- part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
+++ part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
@@ -1,27 +1,48 @@
 
+import java.util.ArrayList;
+
 public class Statistics {
-    private int count = 0;
-    private int sum = 0;
 
-    public Statistics(){
-        int count;
-        int sum;
+    private int count;
+    private int sum;
+
+    private final ArrayList<Integer> list = new ArrayList<>();
+
+    public Statistics() {
+        this.count = 0;
     }
 
-    public int getCount(){
-        return count;
+    public void addNumber(int number) {
+        this.count += 1;
+        this.list.add(number);
+
+        int total = 0;
+        for (int i = 0; i < list.size(); i++) {
+            total += list.get(i);
+        }
+        this.sum = total;
     }
 
-    public void addNumber(int number){
-        this.count++;
-        sum += number;
+    public int getCount() {
+        return this.count;
     }
 
-    public int sum(){
-        return sum;
+    public int sum() {
+        if (this.count == 0) {
+            return 0;
+        } else {
+            return this.sum;
+        }
     }
 
-    public double average(){
-        return (double) sum / count;
+    public double average() {
+        double average;
+        if (this.count == 0) {
+            average = 0;
+        } else {
+            average = 1.0 * this.sum / this.count;
+        }
+
+        return average;
     }
 }
