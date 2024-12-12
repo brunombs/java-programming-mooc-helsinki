edc46347cf995ac2978101e8b8ffbc0e9826938d
brunombs <brunozs@msn.com>
2024-12-12 07:58:34 -0300

    adding exercise

--- part04-Part04_16.PaymentCard/src/main/java/MainProgram.java
+++ part04-Part04_16.PaymentCard/src/main/java/MainProgram.java
@@ -4,14 +4,13 @@ public class MainProgram {
     public static void main(String[] args) {
         // Scanner scanner = new Scanner(System.in);
         
-        PaymentCard paymentCard = new PaymentCard(50);
-        System.out.println(paymentCard);
-
-        paymentCard.eatAffordably();
+        PaymentCard paymentCard = new PaymentCard(5);
         System.out.println(paymentCard);
 
         paymentCard.eatHeartily();
-        paymentCard.eatAffordably();;
+        System.out.println(paymentCard);
+
+        paymentCard.eatHeartily();
         System.out.println(paymentCard);
 
     }
