ef57154f66bb0efcedecd6ef8a6781d99e9df886
brunombs <brunozs@msn.com>
2024-12-12 07:18:19 -0300

    adding exercise

--- part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
+++ part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
@@ -1,3 +1,16 @@
 
 public class Statistics {
+    private int count;
+
+    public Statistics(){
+        int numberCount;
+    }
+
+    public int getCount(){
+        return count;
+    }
+
+    public void addNumber(int number){
+        this.count++;
+    }
 }
