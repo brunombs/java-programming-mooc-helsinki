6ad49ce22b9e1513b2093dcbc7ebc13013e85f80
brunombs <brunozs@msn.com>
2024-12-13 09:04:46 -0300

    adding exercise

--- part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
+++ part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
@@ -5,6 +5,17 @@ public class NumberOfStrings {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int quantityOfStrings = 0;
 
+        while(true){
+            String string = scanner.nextLine();
+            if(string.equals("end")){
+                break;
+            }
+
+            quantityOfStrings++;
+        }
+
+        System.out.println(quantityOfStrings);
     }
 }
