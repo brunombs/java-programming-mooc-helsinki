41db30b0d9238fec3f7516b0c8f9f0473b8b855c
brunombs <brunozs@msn.com>
2024-12-14 02:12:57 -0300

    adding exercise

--- part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
+++ part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
@@ -1,17 +1,33 @@
 
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class IsItInTheFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> lines = new ArrayList<>();
+
 
         System.out.println("Name of the file:");
         String file = scanner.nextLine();
+        
+        try(Scanner scanner2 = new Scanner(Paths.get(file))) {
+            while (scanner2.hasNextLine()) {
+                lines.add(scanner2.nextLine());
+            }
+        } catch (Exception e) {
+            System.out.println("Reading the file " + file + ".txt failed.");
+        }
 
         System.out.println("Search for:");
         String searchedFor = scanner.nextLine();
 
+        if (lines.contains(searchedFor)){
+            System.out.println("Found!");
+        } else if (!lines.contains((searchedFor))){
+            System.out.println("Not found!");
+        }
     }
 }
