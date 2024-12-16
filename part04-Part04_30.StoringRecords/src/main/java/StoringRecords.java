1dafb2765e14fff5ae24164eced0497b566634d1
brunombs <brunozs@msn.com>
2024-12-16 06:24:29 -0300

    adding exercise

--- part04-Part04_30.StoringRecords/src/main/java/StoringRecords.java
+++ part04-Part04_30.StoringRecords/src/main/java/StoringRecords.java
@@ -23,8 +23,20 @@ public class StoringRecords {
     public static ArrayList<Person> readRecordsFromFile(String file) {
         ArrayList<Person> persons = new ArrayList<>();
 
-        // Write here the code for reading from file
-        // and printing the read records
+        try (Scanner scanner = new Scanner(Paths.get(file))) {
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+
+                String[] parts = line.split(",");
+                String name = parts[0];
+                int age = Integer.valueOf(parts[1]);
+
+                persons.add(new Person(name, age));
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
         return persons;
 
     }
