021340838b6b1b9f4844dd5717bfa2cd33f01458
brunombs <brunozs@msn.com>
2024-12-17 04:24:34 -0300

    adding exercise

--- part05-Part05_06.OverloadedCounter/src/main/java/MainProgram.java
+++ part05-Part05_06.OverloadedCounter/src/main/java/MainProgram.java
@@ -2,6 +2,21 @@
 public class MainProgram {
 
     public static void main(String[] args) {
-        // Test your counter here
+        Counter counter = new Counter(5);
+        Counter counterTwo = new Counter();
+
+        System.out.println(counter.value());
+        counter.increase();
+        System.out.println(counter.value());
+        counter.decrease();
+        System.out.println(counter.value());
+
+        System.out.println("---------------");
+        
+        System.out.println(counterTwo.value());
+        counterTwo.increase();
+        System.out.println(counterTwo.value());
+        counterTwo.decrease();
+        System.out.println(counterTwo.value());
     }
 }
