e20c5bd6e45a5ba57f2deae933741aa8f2cdbdc1
brunombs <brunozs@msn.com>
2024-12-16 23:47:52 -0300

    adding exercise

--- part05-Part05_03.Cube/src/main/java/Program.java
+++ part05-Part05_03.Cube/src/main/java/Program.java
@@ -6,6 +6,8 @@ public class Program {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Experiment with your program here
+        Cube cube = new Cube(4);
+        System.out.println(cube.volume());
+        System.out.println(cube);
     }
 }
