6638dacc5b296e598787978544faa4f399331395
brunombs <brunozs@msn.com>
2024-12-17 06:00:14 -0300

    adding exercise

--- part05-Part05_09.HealthStation/src/main/java/HealthStation.java
+++ part05-Part05_09.HealthStation/src/main/java/HealthStation.java
@@ -1,9 +1,11 @@
 
 public class HealthStation {
-
-
     public int weigh(Person person) {
         return person.getWeight();
+    }
+
+    public void feed(Person person) {
+        person.setWeight(person.getWeight() + 1);
     }
 
 }
