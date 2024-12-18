06c595b62673a143b58269aa0a5274622f54e612
brunombs <brunozs@msn.com>
2024-12-18 06:35:54 -0300

    adding exercise

--- part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
+++ part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
@@ -47,6 +47,13 @@ public class PaymentTerminal {
         return false;
     }
 
+    public void addMoneyToCard(PaymentCard card, double sum) {
+        if (sum >= 0){
+        card.addMoney(sum);
+        this.money += sum;
+        }
+    }
+
 
     @Override
     public String toString() {
